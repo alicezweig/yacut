1cf16fd7bef153288a6bd28175a3e82db535af25
Irina Ivanova <ivanova1669@gmail.com>
2024-01-21 20:09:00 +0300

    SQLALCHEMY_DATABASE_URI

--- yacut/settings.py
+++ yacut/settings.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.getenv('SECRET_KEY')
